b62755262041bfd71bc9ee3d07da5dd797546758
Gaetan Bahl <gaetan.bahl@gmail.com>
2024-12-11 08:29:33 +0100

    day 11

---
+++ 11/p1.py
@@ -0,0 +1,32 @@
+import math
+
+N_BLINKS = 25
+    
+def n_digits(s):
+    if s < 10:
+        return 1
+    else:
+        return 1 + n_digits(s//10)
+
+def split(s, a):
+    s = str(s)
+    return [int(s[:a]), int(s[a:])]
+
+with open("input", 'r') as f:
+
+    line = f.readlines()[0]
+    stones = list(map(int, line.split()))
+
+    for i in range(N_BLINKS):
+        new_stones = []
+        for s in stones:
+            if s == 0:
+                new_stones.append(1)
+            elif (n := n_digits(s)) % 2 == 0 :
+                new_stones += split(s, n//2)
+            else:
+                new_stones.append(s*2024)
+
+        stones = new_stones
+
+    print(len(stones))
---
+++ 11/p2.py
@@ -0,0 +1,37 @@
+import math
+import sys
+from functools import cache
+
+sys.setrecursionlimit(1000)
+
+N_BLINKS = 75
+    
+def n_digits(s):
+    if s < 10:
+        return 1
+    else:
+        return 1 + n_digits(s//10)
+
+def split(s, a):
+    s = str(s)
+    return (int(s[:a]), int(s[a:]))
+
+@cache
+def comp(s, depth):
+
+    if depth == 0:
+        return 1
+    else:
+        if s == 0:
+            return comp(1, depth-1)
+        elif (n := n_digits(s)) % 2 == 0 :
+            s1, s2 = split(s, n//2)
+            return comp(s1, depth-1) + comp(s2, depth-1)
+        else:
+            return comp(s*2024, depth-1)
+
+
+with open("input", 'r') as f:
+
+    stones = f.readlines()[0].split()
+    print(sum([comp(int(s), N_BLINKS) for s in stones]))
